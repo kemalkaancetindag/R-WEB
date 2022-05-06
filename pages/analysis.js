9092e179476a60557e026d0056888d47e6e1ebdc
Kemal Kaan Çetindağ <58605537+kemalkaancetindag@users.noreply.github.com>
2022-05-06 18:03:40 +0300

    download done

--- pages/analysis.js
+++ pages/analysis.js
@@ -162,6 +162,16 @@ export default function Analysis() {
         router.reload()
     }
 
+    const downloadAnalysis = async (e) => {        
+        try{
+            window.open(`${ApiUrl}/analysis/download?analysis_code=${analysisCode}`,'_blank')
+            var response = await axios.get(`${ApiUrl}/analysis/download?analysis_code=${analysisCode}`)
+        }
+        catch(e){
+            console.log(e.toString())
+        }                        
+    }
+
 
 
     return (
@@ -330,8 +340,10 @@ export default function Analysis() {
                                                             </th>
                                                         ))}
 
-                                                        <th scope="col" className="px-6 py-3">
-
+                                                        <th scope="col" className="px-6 py-3 text-right">
+                                                        <button className="bg-white hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" onClick={(e) => downloadAnalysis(e)}>
+                                                                İndir
+                                                        </button>
                                                         </th>
                                                     </tr>
                                                 </thead>
